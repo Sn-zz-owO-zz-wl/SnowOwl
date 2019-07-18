1d8af1b6a1b265771c76205678f24c36dd178a1c
Sn-zz-owO-zz-wl <keffear@gmail.com>
2019-07-19 02:06:19 +0500

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -10,61 +10,30 @@ function highlight(table) {
 
 
     for (let i = 0; i < rows.length; i++) {
-      let statusCell = rows[i].cells[3];
-        console.log(rows[i]);
-      if (statusCell.dataset.available === 'true') {
+      
+    let statusCell = rows[i].cells[3];
+
+        if (statusCell.dataset.available === 'true') {
         rows[i].className = 'available';
-      } else if (statusCell.dataset.available === 'false') {
+        } else if (statusCell.dataset.available === 'false') {
           rows[i].className = 'unavailable';
-      } else rows[i].setAttribute('hidden', 'hidden');
-    }
-
-
-
-    for (let j = 0; j < rows.length; j++) {
-        let genderCell = rows[j].cells[2];
+        } else rows[i].setAttribute('hidden', 'hidden');
+    
+    let genderCell = rows[i].cells[2];
 
         if (genderCell.textContent === 'm') {
-            rows[j].className = 'male';
-        } else  rows[j].className = 'female';
-    }
+        rows[i].className = 'male';
+        } else  rows[i].className = 'female';
 
 
-    for (let k = 0; k < rows.length; k++) {
-        let ageCell = rows[k].cells[1];
-
+    let ageCell = rows[i].cells[1];
+        
         if (ageCell.textContent < 18) {
-            rows[k].style="text-decoration: line-through";
+        rows[i].style="text-decoration: line-through";
         }
     }
+}   
 
-}
 
 highlight(document.querySelector('.js-teachers'));
 
-/*
-
-'use strict';
-/!*
-
-function highlight(table) {
-    for (let i = 0; i < cells.length; i++) {
-        let a = cells[i];
-        if status
-        a.classList.add('available');
-    }
-}
-new_row.className = "aClassName";
-*!/
-
-
-
-let elems = document.getElementsByTagName('td');
-alert(elems);
-for (let i = 0; i < elems.length; i++) {
-    let statusCell = elems[i].cell[2];
-    alert (statusCell);
-    if (elems.i === 'data-available') {
-        console.log('true');
-    } else console.log(false);
-}*/
