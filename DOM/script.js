e16e89499f54fc27fc2c9954585a34b919265985
Sn-zz-owO-zz-wl <keffear@gmail.com>
2019-07-22 20:27:40 +0500

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -5,28 +5,25 @@
 function highlight(table) {
 
 
-    let tbody = table.getElementsByTagName('tbody')[0];
-    let rows = tbody.getElementsByTagName('tr');
+    let tbody = table.querySelector('tbody');
+    let rows = tbody.querySelectorAll('tr');
 
 
     for (let i = 0; i < rows.length; i++) {
       
-    let statusCell = rows[i].cells[3];
+  let [statusCell, genderCell, ageCell] = [rows[i].cells[3], rows[i].cells[2], rows[i].cells[1]];
 
         if (statusCell.dataset.available === 'true') {
         rows[i].className = 'available';
         } else if (statusCell.dataset.available === 'false') {
           rows[i].className = 'unavailable';
         } else rows[i].setAttribute('hidden', 'hidden');
-    
-    let genderCell = rows[i].cells[2];
+   
 
         if (genderCell.textContent === 'm') {
         rows[i].className = 'male';
         } else  rows[i].className = 'female';
 
-
-    let ageCell = rows[i].cells[1];
         
         if (ageCell.textContent < 18) {
         rows[i].style="text-decoration: line-through";
